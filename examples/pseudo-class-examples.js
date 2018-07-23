5edbb92cf12eaa247fff815557ea195460b1d635
Tommy Hodgins <tomhodgins@gmail.com>
2018-07-22 23:18:19 -0400

    Add :attr-greater-than() and :attr-less-than() and element query pseudo-classes

--- examples/pseudo-class-examples.js
+++ examples/pseudo-class-examples.js
@@ -80,26 +80,6 @@ export const containsText = new CustomPseudoClass({
   selector: '${selector}'
 })
 
-// :min-width()
-export const minWidth = new CustomPseudoClass({
-  name: 'minWidth',
-  args: ['selector', 'number'],
-  match: 'selector',
-  filter: 'tag.offsetWidth >= number',
-  target: 'tag',
-  selector: '${selector}'
-})
-
-// :max-width()
-export const maxWidth = new CustomPseudoClass({
-  name: 'maxWidth',
-  args: ['selector', 'number'],
-  match: 'selector',
-  filter: 'tag.offsetWidth <= number',
-  target: 'tag',
-  selector: '${selector}'
-})
-
 // :excludes-regex()
 export const excludesRegex = new CustomPseudoClass({
   name: 'excludesRegex',
@@ -147,6 +127,246 @@ export const inViewport = new CustomPseudoClass({
         && tag.getBoundingClientRect().bottom - tag.offsetHeight <= innerHeight
     }[option]
   `,
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :overflowed()
+export const overflowed = new CustomPseudoClass({
+  name: 'overflowed',
+  args: ['selector', 'options'],
+  match: 'selector',
+  filter: `
+    options.every(option => ({
+      top: tag.scrollTop > 0,
+      right: tag.scrollLeft + tag.offsetWidth < tag.scrollWidth,
+      bottom: tag.scrollTop + tag.offsetHeight < tag.scrollHeight,
+      left: tag.scrollLeft > 0
+    })[option])
+  `,
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :min-aspect-ratio()
+export const minAspectRatio = new CustomPseudoClass({
+  name: 'minAspectRatio',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number <= tag.offsetWidth / tag.offsetHeight',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :max-aspect-ratio()
+export const maxAspectRatio = new CustomPseudoClass({
+  name: 'maxAspectRatio',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number >= tag.offsetWidth / tag.offsetHeight',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :min-characters()
+export const minCharacters = new CustomPseudoClass({
+  name: 'minCharacters',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: `
+    number <= (
+      (tag.value && tag.value.length)
+      || tag.textContent.length
+    )
+  `,
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :characters()
+export const characters = new CustomPseudoClass({
+  name: 'characters',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: `
+    number === (
+      (tag.value && tag.value.length)
+      || tag.textContent.length
+    )
+  `,
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :max-characters()
+export const maxCharacters = new CustomPseudoClass({
+  name: 'maxCharacters',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: `
+    number >= (
+      (tag.value && tag.value.length)
+      || tag.textContent.length
+    )
+  `,
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :min-children()
+export const minChildren = new CustomPseudoClass({
+  name: 'minChildren',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number <= tag.children.length',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :children()
+export const children = new CustomPseudoClass({
+  name: 'children',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number === tag.children.length',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :max-children()
+export const maxChildren = new CustomPseudoClass({
+  name: 'maxChildren',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number >= tag.children.length',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :min-height()
+export const minHeight = new CustomPseudoClass({
+  name: 'minHeight',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number <= tag.offsetHeight',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :max-height()
+export const maxHeight = new CustomPseudoClass({
+  name: 'maxHeight',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number >= tag.offsetHeight',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :min-scroll-x()
+export const minScrollX = new CustomPseudoClass({
+  name: 'minScrollX',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number <= tag.scrollLeft',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :max-scroll-x()
+export const maxScrollX = new CustomPseudoClass({
+  name: 'maxScrollX',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number >= tag.scrollLeft',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :min-scroll-y()
+export const minScrollY = new CustomPseudoClass({
+  name: 'minScrollY',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number <= tag.scrollTop',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :max-scroll-y()
+export const maxScrollY = new CustomPseudoClass({
+  name: 'maxScrollY',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number >= tag.scrollTop',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :min-width()
+export const minWidth = new CustomPseudoClass({
+  name: 'minWidth',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number <= tag.offsetWidth',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :max-width()
+export const maxWidth = new CustomPseudoClass({
+  name: 'maxWidth',
+  args: ['selector', 'number'],
+  match: 'selector',
+  filter: 'number >= tag.offsetWidth',
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :orientation()
+export const orientation = new CustomPseudoClass({
+  name: 'orientation',
+  args: ['selector', 'option'],
+  match: 'selector',
+  filter: `
+    {
+      portrait: tag.offsetWidth < tag.offsetHeight,
+      square: tag.offsetWidth === tag.offsetHeight,
+      landscape: tag.offsetWidth > tag.offsetHeight
+    }[option]
+  `,
+  target: 'tag',
+  selector: '${selector}'
+})
+
+// :attr-greater-than()
+export const attrGreater = new CustomPseudoClass({
+  name: 'attrGreater',
+  args: ['selector', 'attribute', 'number'],
+  match: 'selector',
+  filter: `
+    parseInt(
+      attribute === 'value'
+      ? tag.value
+      : tag.getAttribute
+    ) > number`,
+  target: 'tag',
+  selector: '${selector}'
+})
+console.log(attrGreater)
+
+
+// :attr-less-than()
+export const attrLess = new CustomPseudoClass({
+  name: 'attrLess',
+  args: ['selector', 'attribute', 'number'],
+  match: 'selector',
+  filter: `
+    parseInt(
+      attribute === 'value'
+      ? tag.value
+      : tag.getAttribute
+    ) < number`,
   target: 'tag',
   selector: '${selector}'
 })
